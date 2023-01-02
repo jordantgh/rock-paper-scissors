5e110f201e2bc257bef30b539fefa5aaa0a79101
s1349795 <s1349795@ed.ac.uk>
2023-01-02 22:35:05 +0000

    Add round and game functions (unfinished)

--- game.js
+++ game.js
@@ -1,12 +1,28 @@
 
 function getComputerChoice() {
     let pick = Math.round(Math.random() * 2);
-        switch(pick) {
-            case 0:
-                return "Rock";
-            case 1:
-                return "Paper";
-            case 2:
-                return "Scissors";
-        }
+    switch(pick) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
     }
+}
+
+function playRound(player, cpu) {
+    if (player === cpu) {
+        print("Draw, go again.")
+    }
+
+}
+
+function game() {
+
+} 
+
+// pseudo
+if rock, win condition = scissors
+if paper, win condition = rock
+if scissors, win condition = paper
