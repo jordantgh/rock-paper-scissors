96af66a4cae4cf180a8e7af222ff0e69a02eb149
s1349795 <s1349795@ed.ac.uk>
2023-01-03 00:07:55 +0000

    Try out choices as objects

--- game.js
+++ game.js
@@ -1,21 +1,37 @@
+let cpuSelection = {
+    owner: 'cpu',
+    choice: ''
+}
+
+let playerSelection = {
+    owner: 'player',
+    choice: ''
+}
 
 function getComputerChoice() {
     let pick = Math.round(Math.random() * 2);
     switch(pick) {
         case 0:
-            return "Rock";
+            cpuSelection.choice = 'rock';
+            break;
         case 1:
-            return "Paper";
+            cpuSelection.choice = 'paper';
+            break;
         case 2:
-            return "Scissors";
+            cpuSelection.choice = 'scissors';
+            break;
     }
 }
 
-function playRound(player, cpu) {
-    if (player === cpu) {
-        print("Draw, go again.")
-    }
+function playRound(playerSelection, cpuSelection) {
+    if (playerSelection.choice === cpuSelection.choice) {
+        return "Draw, go again."
+    } else {
+        let pair = [playerSelection.choice, cpuSelection.choice]
+        if (pair.includes("rock") && pair.includes("scissors")) {
 
+        }
+    }
 }
 
 function game() {
